1f013481fff77d21aa4122388e0337b01363e8bd
gootara.org <dev@gootara.org>
2013-11-22 03:52:18 +0900

    Second commit. Not available yet.

---
+++ ios-image-util/src/org/gootara/IOSImageUtil.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2013 gootara.org <http://gootara.org>
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.gootara;
+
+import org.gootara.ui.MainFrame;
+
+/**
+ * @author gootara.org
+ *
+ */
+public class IOSImageUtil {
+
+	public static void main(String[] args) throws Exception {
+		MainFrame mainFrame = new MainFrame();
+		mainFrame.setSize(640, 320);
+		mainFrame.setVisible(true);
+	}
+
+}
